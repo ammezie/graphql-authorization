05c1c729aeafcbe7a00f83a838e0b4bb96ad28fa
Chimezie Enyinnaya <meziemichael@gmail.com>
2018-10-03 14:30:11 +0100

    update to use sqlite

--- config/config.js
+++ config/config.js
@@ -1,13 +1,8 @@
 'use strict'
 
-require('dotenv').config()
-
 const dbDetails = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  dialect: 'mysql'
+  dialect: 'sqlite',
+  storage: './database.sqlite3'
 }
 
 module.exports = {
